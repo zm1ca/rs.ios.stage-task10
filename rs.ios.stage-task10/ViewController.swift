84bdeedaf95081827d2ada922a87a85fd94046c3
zm1ca <zmicier.fied@yandex.ru>
2021-08-25 15:13:29 +0300

    Initial Commit

---
+++ rs.ios.stage-task10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rs.ios.stage-task10
+//
+//  Created by Źmicier Fiedčanka on 25.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
