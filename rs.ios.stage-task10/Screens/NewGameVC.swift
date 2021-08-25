c1b99e078956b43aff49837f436e9f03bf893748
zm1ca <zmicier.fied@yandex.ru>
2021-08-25 18:09:06 +0300

    feat: started working on table view

--- rs.ios.stage-task10/Screens/NewGameVC.swift
+++ rs.ios.stage-task10/Screens/NewGameVC.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class NewGameVC: UIViewController {
     
+    private var playerNames = ["Me", "You"]
+    private var tableView   = UITableView(frame: .zero, style: .plain)
+    
     let startButton: UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .RSGreen
@@ -32,12 +35,28 @@ class NewGameVC: UIViewController {
         return btn
     }()
 
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Game Counter"
         view.backgroundColor = UIColor.RSBackground
+        
+        configureTableView()
         configureCancelButton()
         layoutUI()
+    }
+    
+    
+    // MARK: Configuration
+    private func configureTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate   = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PlayerCell")
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = .RSTable
+        tableView.layer.cornerRadius = 15
+        tableView.setEditing(true, animated: false)
     }
     
     private func configureCancelButton() {
@@ -58,14 +77,65 @@ class NewGameVC: UIViewController {
     }
     
     private func layoutUI() {
+        view.addSubview(tableView)
         view.addSubview(startButton)
         NSLayoutConstraint.activate([
             startButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -65),
             startButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             startButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            startButton.heightAnchor.constraint(equalToConstant: 65)
+            startButton.heightAnchor.constraint(equalToConstant: 65),
+            
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            //tableView.heightAnchor.constraint(equalToConstant: 0),
         ])
     }
 
+}
+
+extension NewGameVC: UITableViewDelegate, UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        playerNames.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath)
+        if indexPath.row == playerNames.count {
+            cell.textLabel?.text = "Add player"
+            return cell
+        } else {
+            cell.textLabel?.text = playerNames[indexPath.row]
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if indexPath.row == playerNames.count {
+            return .insert
+        } else {
+            return .delete
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        
+        let label = UILabel()
+        label.frame     = CGRect.init(x: 16, y: 5, width: headerView.frame.width - 26, height: headerView.frame.height - 10)
+        label.text      = "Players"
+        label.font      = UIFont(name: "Nunito-SemiBold", size: 16) //letter l is rounded wtf
+        label.textColor = .RSLabel
+        
+        headerView.addSubview(label)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        50
+    }
+    
 }
 
