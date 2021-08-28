41e6c754e9987ff0f0963097bcce43d48771f1d1
zm1ca <zmicier.fied@yandex.ru>
2021-08-28 20:10:06 +0300

    fix: set status bar appearance in project settings

--- rs.ios.stage-task10/Supporting Files/AppDelegate.swift
+++ rs.ios.stage-task10/Supporting Files/AppDelegate.swift
@@ -13,18 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        configureStatusBar()
         setupWindow()
         return true
     }
 
-    private func configureStatusBar() {
-        if let statusBar = UIApplication.shared.value(forKey: "statusBar") as? UIView {
-            statusBar.backgroundColor = .RSBackground
-            statusBar.setValue(UIColor.white, forKey: "foregroundColor")
-        }
-    }
-    
     private func setupWindow() {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.tintColor = .white
